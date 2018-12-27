f45e74b0c9c80d044c65b2937a470402c42d06d1
daoxuan <daoxuan@pinduoduo.com>
2018-12-27 10:17:53 +0800

    icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Redis Manager");
         primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/redis.png")));
         primaryStage.show();
     }
 
